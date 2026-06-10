29fd0196016db4e1de2bf42c1ecd478a8831ab2b
zeel991 <darjizeel587@gmail.com>
2026-06-10 19:50:37 +0530

    Removed AccessGate

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { Web3Provider } from "@/components/WalletContextProvider";
-import AccessGate from "@/components/AccessGate";
+// import AccessGate from "@/components/AccessGate";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -62,8 +62,8 @@ export default function RootLayout({
 			<body
 				className={`${inter.variable} ${spaceGrotesk.variable} font-sans bg-[#0a0a0f] text-white antialiased`}
 			>
-				<Web3Provider>
-					<AccessGate>{children}</AccessGate>
+				<Web3Provider>					
+					{children}
 				</Web3Provider>
 			</body>
 		</html>
